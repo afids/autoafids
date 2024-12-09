d5db392c4f27b4e9a47438956e528f02517abd83
Dhananjhay <dhananjhay03@gmail.com>
2024-12-09 11:52:06 -0500

    suppress duplication warning and add bids validation

--- autoafids/run.py
+++ autoafids/run.py
@@ -3,10 +3,23 @@ from pathlib import Path
 
 from snakebids import bidsapp, plugins
 
+import logging
+
+# Set up the root logger for general debug-level logging
+# logging.basicConfig(level=logging.DEBUG)
+
+# Prevent duplicate terminal log outputs from snakemake
+snakemake_logger = logging.getLogger("snakemake.logging")
+snakemake_logger.propagate = False  # Prevent duplicate logs
+snakemake_logger.setLevel(logging.INFO)  # Adjust level if needed
+
 app = bidsapp.app(
     [
         plugins.SnakemakeBidsApp(Path(__file__).resolve().parent),
+        plugins.BidsValidator(),
         plugins.Version(distribution="autoafids"),
+        plugins.CliConfig("parse_args"),
+        plugins.ComponentEdit("pybids_inputs"),
     ]
 )
 
